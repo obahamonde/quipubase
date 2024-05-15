812f749f0b407b42908536d939280be302a2c393
obahamonde <oscarmartinbahamondemunoz@gmail.com>
2024-05-15 03:14:45 +0000

    added `qdoc`

--- quipubase/__init__.py
+++ quipubase/__init__.py
@@ -1,3 +1,7 @@
 from .qapi import create_app
+from .qdoc import QDocument
+from .qembed import EmbeddingAPI
+from .qtools import Tool
+from .qvector import QVector
 
-__all__ = ["create_app"]
+__all__ = ["create_app", "QDocument", "EmbeddingAPI", "Tool", "QVector"]
